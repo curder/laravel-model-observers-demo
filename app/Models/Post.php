3b35be608bef68dca063d054bd11a9962a026276
curder <curder@users.noreply.github.com>
2022-03-23 06:54:08 +0000

    Fix styling

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Observers\PostObserver;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title', 'body',
